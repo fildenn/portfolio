6690065319cd9b89971cbeb41f19621c1a02603c
John Dave Manuel <jdmanuel242@gmail.com>
2016-07-25 14:13:03 +0800

    Add files via upload

---
+++ files/viewhsk.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HSK CERTIFICATE</title>
+   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+   <link href="css/font-awesome.css" rel="stylesheet">
+   <link href="style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+   <link id="switcher" href="css/themes/lite-blue-theme.css" type="text/css" rel="stylesheet" media="screen,projection"/>  
+  <link rel="shortcut icon" type="image/x-icon" href="me.ico"/>
+</head>
+
+<body style = "background-color:#37C6F5;">
+	      <header id="header" role="banner">
+        <div class="navbar-fixed">
+          <nav>
+            <div class="container">
+              <div class="nav-wrapper">
+                <a href="www.jandeybmanuel.esy.es" class="brand-logo">John&nbsp;Dave&nbsp;Manuel</a>
+                </ul>
+                <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+              </div>
+            </div>
+          </nav>
+        </div>  
+      </header>
+      <br>
+      <div style = "text-align:center;">
+
+<iframe src="files/hsk.pdf" scrolling = "auto" frameborder = "0" height = "1015px" width = "800px"></iframe>
+</div>
+ <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+ <script type="text/javascript" src="js/latestmaterialize.min.js"></script>
+ <script src="js/smoothscroll.min.js" type="text/javascript"></script>
+</body>
+</html>
